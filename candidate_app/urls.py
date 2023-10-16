96df3988685587725baac76633a8f95b4a1cbc53
denil <denilkurian@gmail.com>
2023-10-16 15:36:21 +0530

    login

--- candidate_app/urls.py
+++ candidate_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import welcome
+from .views import welcome,signup,signin,user,admin
 
 
 
 urlpatterns = [
-    path('',welcome,name='welcome')
-    
-    
+    path('user/',user,name='user'),
+    path('admin_page/',admin,name='admin'),
+    path('signup/',signup,name='signup'),
+    path('',signin,name='login'),
+   
     
     ]
 
