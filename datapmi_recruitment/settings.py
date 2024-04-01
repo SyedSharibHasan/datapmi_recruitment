d17410676d8d73e40c520a56a31c9a888b2e4201
Denil Kurian <denilkuriancc@gmail.com>
2024-04-01 09:45:10 +0530

    my new

--- datapmi_recruitment/settings.py
+++ datapmi_recruitment/settings.py
@@ -98,27 +98,27 @@ WSGI_APPLICATION = 'datapmi_recruitment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'recruitment',
-        'USER': 'user',
-        'PASSWORD': '',  
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'autocommit': True,
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': 'recruitment',
+#         'USER': 'user',
+#         'PASSWORD': '',  
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'autocommit': True,
+#         },
+#     }
+# }
 
 
 
