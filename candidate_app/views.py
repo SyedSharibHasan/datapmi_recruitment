d3f06447844a6539d3df7beaa9f064d470a929c3
Denil Kurian <denilkuriancc@gmail.com>
2024-04-05 14:50:42 +0530

    excel updation

--- candidate_app/views.py
+++ candidate_app/views.py
@@ -333,7 +333,8 @@ def extract_integer_value(text):
 
 import re
 from io import BytesIO
-
+from django.core.files.uploadedfile import SimpleUploadedFile
+from pathlib import Path
 
 @recruiter_login_required
 def mycandidates(request):
@@ -398,20 +399,13 @@ def mycandidates(request):
                         (row.loc['Expected ctc'] if 'Expected ctc' in row else
                         row.loc['EXPECTED CTC'] if 'EXPECTED CTC' in row else None))
                     )
-                    if row.Resume and isinstance(row.Resume, str) and os.path.exists(row.Resume):
-                        # Extracting file name from the path
-                        resume_file_path = row.Resume
-                        resume_file_name = os.path.basename(resume_file_path)
 
-                        # Creating FileSystemStorage object
-                        fs = FileSystemStorage()
-
-                        # Saving the file to the desired location
-                        with open(resume_file_path, 'rb') as resume_file:
-                            saved_resume = fs.save(resume_file_name, resume_file)
+                    resume_path = row.get('Resume')
+                    if resume_path and Path(resume_path).is_file():
+                        with open(resume_path, 'rb') as resume_file:
+                            saved_resume = SimpleUploadedFile(Path(resume_path).name, resume_file.read())
                     else:
-                        # If there's no resume, set the saved_resume to None
-                        saved_resume = None  
+                        saved_resume = None
 
 
                     total_experience_text = extract_integer_value(total_experience)           
